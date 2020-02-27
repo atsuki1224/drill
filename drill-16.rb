9bde2bbeb01c2d98f71e9e5de98a1d0a140616f3
atsuki1224 <akeve1224@icloud.com>
2020-02-27 19:06:03 +0900

    drill16

---
+++ drill-16.rb
@@ -0,0 +1,25 @@
+class Article
+
+  def initialize(author, title, content)
+    @author = author
+    @title = title
+    @content = content
+  end
+
+  def author
+    @author
+  end
+
+  def title
+    @title
+  end
+
+  def content
+    @content
+  end
+
+end
+  article = Article.new("阿倍", "Rubyは素晴らしい", "Awesome Ruby!")
+  puts "著者：#{article.author}"
+  puts "タイトル：#{article.title}"
+  puts "本文：#{article.content}"
