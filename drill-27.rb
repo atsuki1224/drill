331813271d523416562a67b1540e90d7296ee539
atsuki1224 <akeve1224@icloud.com>
2020-03-01 18:38:43 +0900

    drill27

---
+++ drill-27.rb
@@ -0,0 +1,11 @@
+# if文での記述
+# var = "文字列"
+# if var.class == String
+#   puts "varはStringです"
+# else
+#   puts "varはStringではありません"
+# end
+
+# 三項演算子での記述
+var = "文字列"
+var.class == String ? "varはstringです" : "varはstringではありません"
