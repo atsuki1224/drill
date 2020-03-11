da6e853f314d3058afda63f4374b5f6a9c022ca8
atsuki1224 <akeve1224@icloud.com>
2020-03-11 20:06:24 +0900

    drill58

---
+++ drill-58.rb
@@ -0,0 +1,40 @@
+# 平日でないまたは休日の場合は「True」と返信し、
+# 休日ではない場合は「False」と条件分岐させるメソッドを作りましょう。
+
+# 呼び出し方：
+# sleep_in(weekday, vacation)
+
+# 出力例：
+# sleep_in(false, false) → False
+# sleep_in(true, false) → False
+# sleep_in(false, true) → True
+
+
+def sleep_in(weekday, vacation)
+  if vacation == false
+    puts "False"
+  else
+    puts "True"
+  end
+end
+
+weekday = true
+vacation = false
+sleep_in(weekday, vacation)
+
+# 模範解答
+def sleep_in(is_weekday, is_vacation)
+ if (is_weekday != true) || (is_vacation == false)
+   puts "True"
+ else
+   puts "False"
+ end
+end
+
+
+is_weekday = true
+is_vacation = false
+
+sleep_in(is_weekday,is_vacation)
+
+# これでは平日の際にtrueなので間違えている可能性あり。
