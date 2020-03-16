a54edcf2cefc999a486c8f2b50de49b6be1e4a63
atsuki1224 <akeve1224@icloud.com>
2020-03-16 23:46:59 +0900

    drill61

---
+++ drill-61.rb
@@ -0,0 +1,13 @@
+20時から翌朝7時までにオウムに喋られると問題があるのでその場合は「NG」、
+それ以外は「OK」と出力するメソッドを作成します。
+オウムが喋る時をture、喋らない時をfalseと入力することにし、時刻も同時に入力します。
+
+呼び出し方：
+parrot_trouble(talking, hour)
+
+出力例：
+parrot_trouble(true, 6) → NG
+parrot_trouble(true, 7) → OK
+parrot_trouble(false, 6) → OK
+parrot_trouble(false, 7) → OK
+
