31148519d3c4059cd19b715ad9da4ced17860e63
atsuki1224 <akeve1224@icloud.com>
2020-03-17 23:37:22 +0900

    drill61

--- drill-61.rb
+++ drill-61.rb
@@ -1,13 +1,24 @@
-20時から翌朝7時までにオウムに喋られると問題があるのでその場合は「NG」、
-それ以外は「OK」と出力するメソッドを作成します。
-オウムが喋る時をture、喋らない時をfalseと入力することにし、時刻も同時に入力します。
+# 20時から翌朝7時までにオウムに喋られると問題があるのでその場合は「NG」、
+# それ以外は「OK」と出力するメソッドを作成します。
+# オウムが喋る時をture、喋らない時をfalseと入力することにし、時刻も同時に入力します。
 
-呼び出し方：
-parrot_trouble(talking, hour)
+# 呼び出し方：
+# parrot_trouble(talking, hour)
 
-出力例：
-parrot_trouble(true, 6) → NG
-parrot_trouble(true, 7) → OK
-parrot_trouble(false, 6) → OK
-parrot_trouble(false, 7) → OK
+# 出力例：
+# parrot_trouble(true, 6) → NG
+# parrot_trouble(true, 7) → OK
+# parrot_trouble(false, 6) → OK
+# parrot_trouble(false, 7) → OK
 
+def parot_trouble(talking, hour)
+  if (talking == true) && (hour == 7..19)
+    puts "OK"
+  else
+    puts "NG"
+  end
+end
+
+talking = false
+hour = 7
+parot_trouble(talking, hour)
